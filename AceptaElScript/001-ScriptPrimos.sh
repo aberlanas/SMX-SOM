b22c9742ed1817b4e3f9a37c862db94e3b4a5a92
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2021-02-06 12:51:39 +0100

    UpdateD

--- AceptaElScript/001-ScriptPrimos.sh
+++ AceptaElScript/001-ScriptPrimos.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-:> /tmp/esPrimo.log
-
 INITIAL_NUMBER=1
 
 if [ -z "$1" ];then
@@ -13,34 +10,25 @@ fi
 esPrimo(){
 
     PRIMO_TO_TEST=$1
-    let TEST=2
-    
-    let LIMIT_TO_TEST=$PRIMO_TO_TEST/2-1
-    
-
     NOT_PRIME=""
 
-    echo " ******** " >> /tmp/esPrimo.log
-    echo " ** COMPROBAREMOS : $PRIMO_TO_TEST - hasta ${LIMIT_TO_TEST}" >> /tmp/esPrimo.log
+    let TEST=2
+    let LIMIT_TO_TEST=$PRIMO_TO_TEST/2-1
+    
+    test $LIMIT_TO_TEST -gt 3 || LIMIT_TO_TEST=3
 
     while [ ${TEST} -gt 1 -a ${TEST} -lt ${LIMIT_TO_TEST} -a -z "${NOT_PRIME}" ]; do
     
 
         let module=${PRIMO_TO_TEST}%${TEST}
 
-        if [ $module -eq 0 ]; then
+        if [ $module -eq 0 -a $PRIMO_TO_TEST -ne 2 ]; then
             NOT_PRIME="TRUE"
-            echo " - ${PRIMO_TO_TEST} % ${TEST} : 0 " >> /tmp/esPrimo.log
-        else
-            echo " - ${PRIMO_TO_TEST} % ${TEST} : != 0 " >> /tmp/esPrimo.log
         fi
-
         let TEST=$TEST+1
-        
     done
 
     if [ -z "${NOT_PRIME}" ];then
-        echo " - $PRIMO_TO_TEST NO PRIMO " >> /tmp/esPrimo.log
         echo 0
     else
         echo 1
@@ -51,15 +39,11 @@ PRIMO=1
 
 while [ $nPrimos -gt 0 ]; do
 
-    echo "Calculando : ${PRIMO}"
-
     rc=$(esPrimo $PRIMO)
 
     if [ $rc -eq 0 ]; then
-        echo " $PRIMO : Es primo "
+        echo " $PRIMO "
         let nPrimos=$nPrimos-1
-    else
-        echo " $PRIMO : No es primo "
     fi
 
     let PRIMO=$PRIMO+1
